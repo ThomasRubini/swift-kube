919d73927166167aee5bf1dfabf9cac5aa244ab0
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-12-10 08:31:56 +0100

    add basic parsing code

--- main.swift
+++ main.swift
@@ -1,29 +1,55 @@
 import Foundation
 
+struct Node {
+    var id: Int
+    var cpu: Int
+    var ram: Int
+    var pods: [Pod]
+}
+
+struct Pod {
+    var id: Int
+    var containers: [Container]
+}
+
+struct Container {
+    var name: String
+    var status: String
+    var cpu: Int
+    var ram: Int
+}
+
 // Récupération du statut du cluster
-func parseClusterStatus(fileContent: String) -> Void {
+func parseClusterStatus(fileContent: String) -> [Node] {
+
+    var nodes = [Node]()
+    var currentNode: Node?
+    var currentPod: Pod?
 
     for line in fileContent.split(separator: "\n") {
         let trimmedLine = line.trimmingCharacters(in: .whitespaces)
 
         // Check for node information
         if trimmedLine.hasPrefix("Node:") {
-            print("un noeud est détecté")
             let nodeId = Int(trimmedLine.split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
-            print("nodeId: \(nodeId)")
+
+            currentNode = Node(id: nodeId, cpu: 0, ram: 0, pods: [])
+            nodes.append(currentNode!)
         } 
         // Check for node resources
         else if trimmedLine.hasPrefix("Ressources:") {
             let resources = trimmedLine.replacingOccurrences(of: "Ressources: ", with: "").split(separator: "|")
             let cpu = Int(resources[0].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
             let ram = Int(resources[1].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
-            print("Ressources de ce noeud : CPU: \(cpu), RAM: \(ram)")
+
+            currentNode!.cpu = cpu
+            currentNode!.ram = ram
         } 
         // Check for pod information
         else if trimmedLine.hasPrefix("Pod:") {
-            print("Un pod est détecté")
             let podId = Int(trimmedLine.split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
-            print("son id est : \(podId)")
+            currentPod = Pod(id: podId, containers: [])
+            currentNode!.pods.append(currentPod!)
         } 
         // Check for container information
         else if trimmedLine.hasPrefix("Container:") {
@@ -32,12 +58,16 @@ func parseClusterStatus(fileContent: String) -> Void {
             let containerStatus = containerDetails[1].split(separator: ":")[1].trimmingCharacters(in: .whitespaces)
             let containerCpu = Int(containerDetails[2].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
             let containerRam = Int(containerDetails[3].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
-            print("Container: \(containerName), Status: \(containerStatus), CPU: \(containerCpu), RAM: \(containerRam)")
+            
+            let container = Container(name: String(containerName), status: String(containerStatus), cpu: containerCpu, ram: containerRam)
+            currentPod!.containers.append(container)
         }
     }
+    return nodes
 }
 
 // Example usage
-if let fileContent = try? String(contentsOfFile: "/Users/vberry/Seafile/sCours/2425/DO3_Algo/Swift_codes/kubernetes_monitoring/kube_status.txt", encoding: .utf8) {
-    parseClusterStatus(fileContent: fileContent)
+if let fileContent = try? String(contentsOfFile: "kube_status.txt", encoding: .utf8) {
+    let res = parseClusterStatus(fileContent: fileContent)
+    print(res)
 }
